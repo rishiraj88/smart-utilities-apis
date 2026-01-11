444df45b649f71de9eba8369a17dab6577c3764c
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-11 16:25:59 +0100

    clean

--- src/main/java/project/demo/controller/CustomerController.java
+++ src/main/java/project/demo/controller/CustomerController.java
@@ -15,7 +15,6 @@ import project.demo.service.Customer.CustomerService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
