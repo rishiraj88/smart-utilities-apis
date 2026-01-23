1663cab5c0013f08db16c05abdb9429fb4361a9d
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-23 21:18:45 +0100

    finish method

--- src/main/java/project/demo/application/ReadingApplicationServiceImpl.java
+++ src/main/java/project/demo/application/ReadingApplicationServiceImpl.java
@@ -3,6 +3,7 @@ package project.demo.application;
 import java.util.List;
 
 import project.demo.application.Interfaces.ReadingApplicationService;
+import project.demo.model.Customer;
 import project.demo.model.Reading;
 import project.demo.service.Customer.CustomerService;
 import project.demo.service.Reading.ReadingService;
@@ -21,8 +22,23 @@ public class ReadingApplicationServiceImpl implements ReadingApplicationService 
 
     @Override
     public Reading createReading(Reading reading) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createReadingWithNewCustomer'");
+
+        Customer customer = new Customer();
+
+        Customer dbCustomer = customerService.getCustomerById(reading.getCustomer().getId());
+
+        if(dbCustomer.getId() == null)
+        {
+            customer = customerService.createCustomer(reading.getCustomer());
+        }
+        else
+        {
+            customer = dbCustomer;
+        }
+
+        reading.setCustomer(customer);
+    
+        return readingService.createReading(reading);
     }
 
     @Override
