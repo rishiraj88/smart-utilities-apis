8669115a8836a879a87a0b9a0c2169802f514dfd
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-25 20:30:50 +0100

    format

--- src/main/java/project/demo/service/Reading/ReadingServiceImpl.java
+++ src/main/java/project/demo/service/Reading/ReadingServiceImpl.java
@@ -8,78 +8,71 @@ import org.springframework.transaction.annotation.Transactional;
 import project.demo.model.Reading;
 import project.demo.repository.ReadingRepository.ReadingRepository;
 
-
 //Service eh um bean spring gerenciado pelo Spring IoC container
 @Service
-public class ReadingServiceImpl implements ReadingService{
-    
+public class ReadingServiceImpl implements ReadingService {
+
     private final ReadingRepository readingRepository;
 
-    public ReadingServiceImpl(ReadingRepository readingRepository)
-    {
+    public ReadingServiceImpl(ReadingRepository readingRepository) {
         this.readingRepository = readingRepository;
     }
 
-
     @Transactional
-    public Reading createReading(Reading reading)
-    {
+    @Override
+    public Reading createReading(Reading reading) {
         Reading createdReading = readingRepository.save(reading);
 
         return createdReading;
     }
-
 
     @Override
     public Reading getReadingById(Long id) {
 
         Reading dbReading = readingRepository.findById(id).orElse(null);
 
-        //toDo: handle not found exception
+        // toDo: handle not found exception
 
         return dbReading;
     }
 
-
     @Override
     public List<Reading> getAllReadings() {
-     
+
         List<Reading> dbReadings = readingRepository.findAll();
 
-        //if (dbReadings.isEmpty())
-        //handle not found exception
+        // if (dbReadings.isEmpty())
+        // handle not found exception
 
         return dbReadings;
     }
 
-
     @Override
     public void deleleteReadingById(Long id) {
-      
+
         Reading dbReading = getReadingById(id);
 
-        //toDo: handle not found exception 
-        //if ( dbReading == null)
+        // toDo: handle not found exception
+        // if ( dbReading == null)
 
         readingRepository.deleteById(id);
     }
 
-
     @Override
     public Reading updateReading(Long id, Reading reading) {
-      
+
         Reading dbReading = getReadingById(id);
 
-        //toDo: handle not found exception
-        //if ( dbReading == null)
+        // toDo: handle not found exception
+        // if ( dbReading == null)
 
         dbReading.setKindOfMeter(reading.getKindOfMeter());
-        dbReading.setComment(reading.getComment()); 
+        dbReading.setComment(reading.getComment());
         dbReading.setMeterId(reading.getMeterId());
         dbReading.setMeterCount(reading.getMeterCount());
         dbReading.setSubstitute(reading.isSubstitute());
         dbReading.setDateOfReading(reading.getDateOfReading());
-        
+
         readingRepository.save(dbReading);
 
         return dbReading;
