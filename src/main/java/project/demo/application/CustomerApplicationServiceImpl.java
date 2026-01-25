734920162bf047628325aea8beeb832f4eb4995b
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-25 20:42:59 +0100

    add transactional propagation

--- src/main/java/project/demo/application/CustomerApplicationServiceImpl.java
+++ src/main/java/project/demo/application/CustomerApplicationServiceImpl.java
@@ -7,6 +7,7 @@ import project.demo.service.Customer.CustomerService;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CustomerApplicationServiceImpl implements CustomerApplicationService {
@@ -18,26 +19,31 @@ public class CustomerApplicationServiceImpl implements CustomerApplicationServic
         this.customerService = customerService;
     }
 
+    @Transactional
     @Override
     public Customer createCustomer(Customer customer) {
         return customerService.createCustomer(customer);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Customer getCustomerById(Long id) {
         return customerService.getCustomerById(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
+    @Transactional
     @Override
     public void deleleteCustomerById(Long id) {
         customerService.deleleteCustomerById(id);
     }
 
+    @Transactional
     @Override
     public Customer updateCustomer(Long id, Customer customer) {
         return customerService.updateCustomer(id, customer);
