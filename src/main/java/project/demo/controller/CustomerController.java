a963c73ff921798959abcb15379cf329eb72e69e
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-26 19:48:19 +0100

    handle exception case

--- src/main/java/project/demo/controller/CustomerController.java
+++ src/main/java/project/demo/controller/CustomerController.java
@@ -1,4 +1,5 @@
 package project.demo.controller;
+
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -16,12 +17,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
-    
+
     private final CustomerApplicationService customerService;
 
     public CustomerController(CustomerApplicationService customerService) {
@@ -29,41 +28,36 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer)
-    {
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
         Customer createdCustomer = customerService.createCustomer(customer);
-        
-        //toDo
-        //handle exceptions and validation
+
+        // toDo
+        // handle exceptions and validation
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
     }
 
- 
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id)
-    {
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
         Customer dbCustomer = customerService.getCustomerById(id);
 
-        //toDo
-        //handle exceptions and validation
+        // toDo
+        // handle exceptions and validation
 
         return ResponseEntity.ok(dbCustomer);
     }
 
     @GetMapping
-    public ResponseEntity<List<Customer>> getCustomers()
-    {
+    public ResponseEntity<List<Customer>> getCustomers() {
         List<Customer> dbCustomers = customerService.getAllCustomers();
 
         return ResponseEntity.ok(dbCustomers);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id)
-    {
-        //todo
-        //handle exceptions and validation id null check ??
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        // todo
+        // handle exceptions and validation id null check ??
 
         customerService.deleleteCustomerById(id);
 
@@ -71,10 +65,13 @@ public class CustomerController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer)
-    {
-        //toDo
-        //handle exceptions and validation id and customer null check ??
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        if (customer == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        // toDo
+        // handle exceptions and validation id and customer null check ??
 
         Customer updatedCustomer = customerService.updateCustomer(id, customer);
 
