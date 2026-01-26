d9c12bba9c712ee0ec5cfa41f5543920a972164a
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-26 20:53:00 +0100

    fix

--- src/main/java/project/demo/controller/dto/CustomerDto.java
+++ src/main/java/project/demo/controller/dto/CustomerDto.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public record CustomerDto(
     Long id,
-    String firsName,
+    String firstName,
     String lastName,
     String gender,
     LocalDate birthDate
