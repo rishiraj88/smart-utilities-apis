b13565d1831d2f8ba9858ed22f877280513d5c4e
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-26 15:41:16 +0100

    fix user type

--- src/main/java/project/demo/infrastructure/repository/user/UserRepository.java
+++ src/main/java/project/demo/infrastructure/repository/user/UserRepository.java
@@ -2,7 +2,8 @@ package project.demo.infrastructure.repository.user;
 
 import java.util.Optional;
 
-import org.apache.catalina.User;
+// import org.apache.catalina.User;
+import project.demo.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
